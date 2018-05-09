6b8b2d2a99e7b1db93eb861148b10373f3ba921a
Natan Grando <natangrando@gmail.com>
2018-05-09 15:34:40 -0300

    Add retrofit helper

---
+++ app/src/main/java/com/natan/choquedecultura/RetrofitHelper.kt
@@ -0,0 +1,50 @@
+package com.natan.choquedecultura
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+
+    private var retrofit: Retrofit? = null
+
+    fun getRetrofit(): Retrofit? {
+        if (retrofit == null) {
+            rebuildRetrofit()
+        }
+        return retrofit
+    }
+
+    private fun rebuildRetrofit() {
+        val client = buildClient()
+        retrofit = Retrofit.Builder()
+                .baseUrl("YOUR_BASE_URL") // TODO: replace with your base url
+                .addConverterFactory(GsonConverterFactory.create(buildGson()))
+                .client(client)
+                .build()
+    }
+
+    private fun buildClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        addLoggingInterceptor(builder)
+        return builder.build()
+    }
+
+    private fun addLoggingInterceptor(builder: OkHttpClient.Builder) {
+        if (BuildConfig.DEBUG) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor { message -> Log.d("HttpLoggingInterceptor", message) }
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            builder.addNetworkInterceptor(httpLoggingInterceptor)
+        }
+    }
+
+    private fun buildGson(): Gson {
+        return GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create()
+    }
+}

54a1eddce9439664bbd7a041718eeb249d2d2cbb
Marcio <marcio@ckl.io>
2018-05-09 18:08:13 -0300

    Fix resource Id not exposed

--- app/src/main/java/com/natan/choquedecultura/models/YoutubeResourceId.kt
+++ app/src/main/java/com/natan/choquedecultura/models/YoutubeResourceId.kt
@@ -1,7 +1,12 @@
 package com.natan.choquedecultura.models
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 class YoutubeResourceId {
 
+    @Expose
+    @SerializedName("videoId")
     var videoId: String? = null
 
     /*

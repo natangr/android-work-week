032015380bfa4aa274e333c0ab2ce071600602d6
Marcio <marcio@ckl.io>
2018-05-09 17:55:06 -0300

    Create list adapter

--- app/src/main/java/com/natan/choquedecultura/MainActivity.kt
+++ app/src/main/java/com/natan/choquedecultura/MainActivity.kt
@@ -3,29 +3,42 @@ package com.natan.choquedecultura
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
+import com.natan.choquedecultura.models.YoutubeItem
 import com.natan.choquedecultura.models.YoutubeResult
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity: AppCompatActivity() {
 
+    val adapter = YoutubeAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = adapter
+
         fetchYoutubeVideos()
     }
 
-    fun fetchYoutubeVideos() {
+    private fun fetchYoutubeVideos() {
         val api = RetrofitHelper.getRetrofit()?.create(YoutubeApi::class.java)
         api?.listVideos()?.enqueue(object: Callback<YoutubeResult> {
             override fun onFailure(call: Call<YoutubeResult>?, t: Throwable?) {
                 Log.e("Youtube", "Error", t)
+                Toast.makeText(this@MainActivity, "Erro!", Toast.LENGTH_LONG).show()
             }
 
             override fun onResponse(call: Call<YoutubeResult>?, response: Response<YoutubeResult>?) {
-                Log.d("Youtube", response?.body()?.items?.firstOrNull()?.snippet?.title)
+                refreshList(response?.body()?.items)
             }
         })
+    }
+
+    private fun refreshList(items: List<YoutubeItem>?) {
+        adapter.items = items ?: emptyList()
     }
 }
---
+++ app/src/main/java/com/natan/choquedecultura/YoutubeAdapter.kt
@@ -0,0 +1,41 @@
+package com.natan.choquedecultura
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.natan.choquedecultura.models.YoutubeItem
+import kotlinx.android.synthetic.main.item_youtube_cell.view.*
+
+class YoutubeAdapter(): RecyclerView.Adapter<YoutubeViewHolder>() {
+
+    var items: List<YoutubeItem> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YoutubeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_youtube_cell, parent, false)
+        return YoutubeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: YoutubeViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+}
+
+class YoutubeViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+
+    fun bind(item: YoutubeItem) {
+        //TODO set the image
+        view.titleTextView.text = item.snippet?.title
+        view.descriptionTextView.text = item.snippet?.description
+    }
+
+}
